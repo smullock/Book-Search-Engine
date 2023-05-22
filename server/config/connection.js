ad2fb6085bf6506c2af47bd535a5ecc0146d7dec
smullock <smullock@hotmail.com>
2023-05-22 14:00:01 +1000

    update connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://smullock:1TaP54Gf7jsGbZ0E@cluster0.zvkwjl0.mongodb.net/googlebooks');
+ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
+
+
