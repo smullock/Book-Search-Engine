7eaec4dbd3e388aa5b237ee3ffd2844ac48d5373
smullock <117344802+smullock@users.noreply.github.com>
2023-05-23 18:12:20 +1000

    Update connection.js
    remove mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-// mongoose.connect('mongodb+srv://smullock:1TaP54Gf7jsGbZ0E@cluster0.zvkwjl0.mongodb.net/googlebooks');
+
 
 
 module.exports = mongoose.connection;
